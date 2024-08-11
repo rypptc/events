b5176dac270f543ee80db7027f3bb368d9ae170c
rypptc <rypptc@gmail.com>
2024-08-11 16:16:00 -0400

    Update App.js add text input, style for iOS and Android

--- events-app/App.js
+++ events-app/App.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
-    <SafeAreaView>
-      <Text style={{fontSize:32}}>hello world</Text>
-      <Text style={{fontSize:24}}>Platform: {Platform.OS === 'ios' ? 'iOS' : 'Android'}</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <Text style={{ fontSize: 32 }}>hello world</Text>
+      <Text style={{ fontSize: 24 }}>Platform: {Platform.OS === 'ios' ? 'iOS' : 'Android'}</Text>
       <ScrollView>
-        <View style={{backgroundColor: 'pink', height: 1600, padding: 20}}>
-          <Text>hello world</Text>
+        <View style={{ backgroundColor: 'pink', height: 1600, padding: 20 }}>
+          <TextInput 
+            defaultValue={text}
+            onChangeText={txt => setText(txt)}
+          />
         </View>
       </ScrollView>
       <StatusBar style="dark" />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0,  // Adds padding for Android devices
+  },
+});
