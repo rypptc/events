6e7f3a9c08beaa1f3ce3224bf61dc5d9b42dbc43
rypptc <rypptc@gmail.com>
2024-08-11 20:02:30 -0400

    add navigation to home screen with a button that takes you to the event screen

--- events-app/screens/home-screen.js
+++ events-app/screens/home-screen.js
@@ -1,9 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.screen}>
       <Text>This is the home screen</Text>
+      <Button title="move to detail" onPress={() => navigation.navigate("Event")} />
     </View>
   );
 }
