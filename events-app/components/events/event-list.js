41a9cb41aee21202251c93dec95a86abf44de04f
rypptc <rypptc@gmail.com>
2024-08-11 20:23:10 -0400

    update event list template, use flat list to display events, add RefreshControl

--- events-app/components/events/event-list.js
+++ events-app/components/events/event-list.js
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, FlatList, RefreshControl } from "react-native";
+import { DUMMY_DATA } from "../../data/dummy";
 
 const EventList = () => {
+    const renderItem = ({ item }) => {
+        return (
+            <View style={{ padding: 40 }}>
+                <Text>{item.title}</Text>
+            </View>
+        );
+    };
   return (
     <View>
-      <Text>event list</Text>
+      <FlatList
+        data={DUMMY_DATA}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+        // numColumns={2}
+        refreshControl={
+            <RefreshControl
+              refreshing={false}
+              onRefresh={() => console.log('refreshing...')}
+            />
+          }
+      />
     </View>
   );
-}
+};
 
 export default EventList;
