fb83bdcde64941e6fc613f16de2668c9444db3c4
rypptc <rypptc@gmail.com>
2024-08-13 00:15:40 -0400

    add image to drawer navigation

--- events-app/navigation/drawer.js
+++ events-app/navigation/drawer.js
@@ -1,13 +1,26 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { HomeStack } from './stack';
-import { ProfileStack } from './stack';  // Ensure this import is correct
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
+import { SafeAreaView, View, Image } from 'react-native'; // Import necessary components
+import { HomeStack, ProfileStack } from './stack';
 
 const Drawer = createDrawerNavigator();
 
 export const MyDrawer = () => {
   return (
     <Drawer.Navigator
-      screenOptions={{headerShown: false}}
+      drawerContent={(props) => {
+        return (
+          <SafeAreaView style={{ flex: 1, paddingTop: 20, backgroundColor: '#99f6e4' }}>
+            <View style={{ justifyContent: 'center', alignItems: 'center', height: 140 }}>
+              <Image 
+                style={{ height: 100, width: 100, resizeMode: 'contain' }} 
+                source={require('../assets/images/logos/logo.png')} 
+              />
+            </View>
+            <DrawerItemList {...props} />
+          </SafeAreaView>
+        );
+      }}
+      screenOptions={{ headerShown: false }}
     >
       <Drawer.Screen 
         name="HomeStack" 
