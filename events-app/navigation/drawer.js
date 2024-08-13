93911d1d04d48edfe5b2cb8bdc2314ceb3cba1f1
rypptc <rypptc@gmail.com>
2024-08-13 12:16:52 -0400

    add icons to drawer items

--- events-app/navigation/drawer.js
+++ events-app/navigation/drawer.js
@@ -1,5 +1,7 @@
 import { createDrawerNavigator, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
 import { SafeAreaView, View, Image, Linking } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { HomeStack, ProfileStack } from './stack';
 
 const Drawer = createDrawerNavigator();
@@ -20,6 +22,9 @@ export const MyDrawer = () => {
             <DrawerItem
               label="More info"
               onPress={() => Linking.openURL('https://google.com')}
+              icon={() => (
+                <Ionicons name="information" size={22} />
+              )}
             />
           </SafeAreaView>
         );
@@ -29,12 +34,18 @@ export const MyDrawer = () => {
       <Drawer.Screen 
         name="HomeStack" 
         component={HomeStack} 
-        options={{ title: "Home" }} 
+        options={{ 
+          title: "Home",
+          drawerIcon: () => <Ionicons name="home" size={22} />
+        }} 
       />
       <Drawer.Screen 
         name="ProfilesStack" 
         component={ProfileStack} 
-        options={{ title: 'Profiles' }} 
+        options={{ 
+          title: 'Profiles',
+          drawerIcon: () => <MaterialCommunityIcons name="face-man-profile" size={22} />
+        }}
       />
     </Drawer.Navigator>
   );
