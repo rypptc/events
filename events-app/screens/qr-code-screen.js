873ef3944ca98fc54eebd4efdf0fdf33bcc9e896
rypptc <rypptc@gmail.com>
2024-08-15 12:35:07 -0400

    implement qr-code-screen

--- events-app/screens/qr-code-screen.js
+++ events-app/screens/qr-code-screen.js
@@ -1,16 +1,52 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner'
+
 
 const QrCodeScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+    if (data) {
+      // use the navigation to navigate to the detail event page
+    }
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.screen}>
-      <Text>qr code skan</Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
+
 };
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 20,
+    flex: 1,
   },
 });
 
