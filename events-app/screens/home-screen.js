b5c6c44d31c2ee9cd5e5af9fb638816d3aee73d7
rypptc <rypptc@gmail.com>
2024-08-14 21:47:40 -0400

    update HomeSreen to go with the tutorial

--- events-app/screens/home-screen.js
+++ events-app/screens/home-screen.js
@@ -1,9 +1,7 @@
-import { useNavigation } from "@react-navigation/native";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
 import EventList from "../components/events/event-list";
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
   return (
     <View style={styles.screen}>
       <EventList />
